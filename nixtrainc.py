09aa591b14ce109c72a4e393d2d6416d7ab26f32
joshbaptiste <joshbaptiste@yahoo.com>
2014-01-16 19:54:38 -0500

    renamed to reflect client side process

---
+++ nixtrainc.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import json
+import subprocess
+import blessings
+from bottle import route, run, debug, request, post
+
+
+@post('nixtrain/questions')
+def get_question():
+    question = request.POST.get('question')
+    return question
+
+
+def process_command(command):
+    ret = subprocess.check_call(command, shell=True)
+    if ret:
+        return False
+
+
+def send_question():
+    """
+        Send a new question
+    """
+    pass
+
+def process_question():
+    out = json.loads(get_question())
+    questions = out.keys()
+    if 'command' in questions:
+            if not process_command(out['command']):
+                send_question()
+
+    if 'question' in questions:
+            terminal_output(out['question'])
+
+
+def terminal_output(question):
+    blessings.Terminal(question)
+
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=5454)
